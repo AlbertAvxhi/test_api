34325432f629f98b3db3ef1f3c3ff3abd65b521a
AlbertAvxhi <aavxhi@gmail.com>
2024-09-15 20:21:30 +0200

    Update script.js
    3000sc

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,7 @@ let tbody=document.getElementById("tbody")
 
 
 //fetch function
-fetch("https://localhost:3001/prot")
+fetch("https://localhost:3000/prot")
     .then(res => res.json())
     .then(json =>{
         json.map(data => {
