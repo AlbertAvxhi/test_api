46224aa5ff5cc263cf0c30aebc7036d0a54f6320
AlbertAvxhi <aavxhi@gmail.com>
2024-09-15 15:24:58 +0200

    test3 api

---
+++ frontend/script.js
@@ -0,0 +1,29 @@
+
+
+let tbody=document.getElementById("tbody")
+
+
+
+//fetch function
+fetch("http://localhost:3000/prot")
+    .then(res => res.json())
+    .then(json =>{
+        json.map(data => {
+        console.log(data)
+        tbody.append(td_fun(data));
+        })
+    })
+
+//create table data
+function td_fun({Nr1, prt1, institucioni, nr2, pr2, lenda}) {
+    let td=document.createElement('tr');
+    td.innerHTML= `
+        <td>${Nr1}</td>
+        <td>${prt1}</td>
+        <td>${institucioni}</td>
+        <td>${nr2}</td>
+        <td>${pr2}</td>
+        <td>${lenda}</td>
+    `;
+    return td;
+}
