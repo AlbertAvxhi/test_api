e0005f8f44e9d4ae5f28c676a2f5e78650b9f542
AlbertAvxhi <aavxhi@gmail.com>
2024-09-15 17:06:11 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,7 @@ let tbody=document.getElementById("tbody")
 
 
 //fetch function
-fetch("https://test-api-xv78.onrender.com/prot")
+fetch("http://localhost:3001/prot")
     .then(res => res.json())
     .then(json =>{
         json.map(data => {
